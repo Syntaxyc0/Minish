0e28e5851cc58a03b18df28765621c6746e118e5
Jean BESNIER <jbesnier@e3r2p16.clusters.42paris.fr>
2022-10-20 17:10:58 +0200

    main pour gregouille ma couille

---
+++ tmp_main.c
@@ -0,0 +1,39 @@
+#include "minishell.h"
+
+
+int	g_exit_status;
+
+int main(int argc, char **argv, char **env)
+{
+	t_mini *mini;
+	char	*line;
+
+	g_exit_status = 0;
+	mini = init_mini();
+	get_env(env, mini);
+	(void)argc;
+	(void)argv;
+	while(1)
+	{
+		line = readline("Minishell: ");
+		if (!line)
+			break;
+		parse_token(mini, line);
+		if (check_tokens(mini))
+			free_tokens(mini);
+		get_redir_types(mini);
+		if (check_syntax(mini))
+			free_tokens(mini);
+		parse_spaces(mini);
+		expander(mini);
+		parse_spaces(mini);
+		if (remove_quotes(mini))
+			free_mini_exit_msg(mini, ERR_MALLOC);
+		if (parse_exec_form(mini))
+			free_mini_exit_msg(mini, NULL);
+		if (line)
+			add_history(line);
+		free_commands(mini);
+	}
+	free_mini(mini);
+}
